086f56fe42dceaa7d2d65c957bbf80aa8d05bab2
Angelo <racering14@gmail.com>
2022-11-01 23:20:35 -0300

    Ya fue ya

--- main.c
+++ main.c
@@ -1,36 +1,36 @@
+//This code was written by Angelo Patricio Orellana Aguilera(https://github.com/Faithgel) and Daniel Sebastian Gonzalez Castillo
+//repository: https://github.com/Faithgel/spreedsheet
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Create a typedef for a enum which will be used for represent the column (A,B,C,D,E,F,G,H,I,J), staring from 0 to 9
+
 typedef enum {A,B,C,D,E,F,G,H,I,J} col;
-//create string type
-typedef char* string;
-//Create a struct to hold the data for each cell
+
 typedef union {
     double i;
-    string s;
+    char *s;
 } data;
 typedef struct {
     data d;
     int type;
 } cell;
 
-//Create a matrix to hold the data as matrix of cells (2D array) using the struct cell and tripple pointer to access the first element of the matrix
-//function to create a matrix with dynamic memory allocation and return the pointer to the matrix
-cell*** create_matrix(int rows, int cols){
+cell*** crearPlanilla(int rows, int cols){
     cell*** matrix = (cell***)malloc(rows * sizeof(cell**));
-    for(int i=0; i<rows; i++){
+    //declare i and j to use in the for loop
+    int i,j;
+    //create a for loop to create the rows
+    for(i = 0; i < rows; i++){
         matrix[i] = (cell**)malloc(cols * sizeof(cell*));
-        for(int j=0; j<cols; j++){
+        //create a for loop to create the columns
+        for(j = 0; j < cols; j++){
             matrix[i][j] = (cell*)malloc(sizeof(cell));
         }
     }
     return matrix;
 }
 
-//create a function to input the data to the matrix and return void, this function takes as arguments the col enum/charecter and the row number and, the type of the data (int) and the matrix
-//0= double , 1= text, 2= formula
-void input_data(col c, int r, int type, cell*** matrix) {
+void ingresarDato(col c, int r, int type, cell*** matrix) {
     if(type == 0) {
         double d;
         scanf("%lf", &d);
@@ -38,23 +38,24 @@ void input_data(col c, int r, int type, cell*** matrix) {
         matrix[r][c]->type = type;
     }
     else if(type == 1) {
-        string s = (string)malloc(sizeof(char)*100);
+        char *s = (char*)malloc(sizeof(char)*100);
         scanf("%s", s);
         matrix[r][c]->d.s = s;
         matrix[r][c]->type = type;
     }
     else if(type == 2) {
-        string s = (string)malloc(sizeof(char)*100);
+        char *s = (char*)malloc(sizeof(char)*100);
         scanf("%s", s);
         matrix[r][c]->d.s = s;
         matrix[r][c]->type = type;
     }
 }
-//create the function to get data from the matrix, this function takes as arguments the col enum/character, the row number and void* data type to return the data
-// and return the data of the cell
-void* get_data(col c, int r, cell*** matrix) {
+
+char* obtenerDato(col c, int r, cell*** matrix) {
     if(matrix[r][c]->type == 0) {
-        return &matrix[r][c]->d.i;
+        char *s = (char*)malloc(sizeof(char)*100);
+        sprintf(s, "%lf", matrix[r][c]->d.i);
+        return s;
     }
     else if(matrix[r][c]->type == 1) {
         return matrix[r][c]->d.s;
@@ -64,27 +65,11 @@ void* get_data(col c, int r, cell*** matrix) {
     }
     return NULL;
 }
-//print the grid using get_data function
-void print_grid(cell*** matrix, int* size) {
-for(int i=0; i<size[0]; i++) {
-        for(int j=0; j<size[1]; j++) {
-            if(matrix[i][j]->type == 0) {
-                printf("%lf ", *(double*)get_data(j, i, matrix));
-            }
-            else if(matrix[i][j]->type == 1) {
-                printf("%s ", (char*)get_data(j, i, matrix));
-            }
-            else if(matrix[i][j]->type == 2) {
-                printf("%s ", (char*)get_data(j, i, matrix));
-            }
-        }
-        printf("\n");
-    }
-}
 //free the memory
 void free_memory(cell*** matrix, int* size) {
-    for(int i=0; i<size[0]; i++) {
-        for(int j=0; j<size[1]; j++) {
+    int i,j;
+    for(i=0; i<size[0]; i++) {
+        for(j=0; j<size[1]; j++) {
             if(matrix[i][j]->type == 1) {
                 free(matrix[i][j]->d.s);
             }
@@ -98,7 +83,7 @@ void free_memory(cell*** matrix, int* size) {
     free(matrix);
 }
 
-//Generate a function to transform the character to the enum value
+
 col char_to_enum(char c) {
     switch(c) {
         case 'A':
@@ -125,7 +110,7 @@ col char_to_enum(char c) {
     return -1;
 }
 
-//Generate a function to transform the enum value to the character
+//function to transform the enum value to the character
 char enum_to_char(col c) {
     switch(c) {
         case A:
@@ -153,147 +138,240 @@ char enum_to_char(col c) {
 }
 
 
-//Generate a function to evaluate the formula expression and return the result as double value
-//parameters: char col, int row, cell*** matrix
-//When the formula is valid, the function will return the result of the formula
-//The formula will be in the form of: =A1+A2 or = A1 + A2 both are valid
-//First at all check the char '=' if it is not equal to '=' return -1 to indicate that the formula is not valid
-//Also check if the data type of the cell is not 2 (formula) return -1 to indicate that the formula is not valid
-// char '=' is not in the enum so we need to check it manually
+double evaluarFormula(char col, int row, cell*** matrix) {
 
-double evaluate_formula(char col, int row, cell*** matrix) {
-    if (matrix[row][char_to_enum(col)]->type != 2) {
-        return -1;
-    }
-    char *formula = strtok(matrix[row][char_to_enum(col)]->d.s, " ");
+    char *formula = strtok(obtenerDato(char_to_enum(col), row, matrix), " ");
     if (formula[0] != '=') {
-        return -1;
-    }
-    formula = formula + 1;
-
-    //copy formula to str1
-    char *str1 = strcpy((char*)malloc(sizeof(char)*100), formula);
-
-    //get the first operand and the operator
-
-    char *operand1 = strtok(str1, "+-*/");
-    printf("operand1: %s\n", operand1);
-    printf("formula: %s\n", formula);
-
-    char *operator = operator = strndup(formula + strlen(operand1), 1);
-
-    //extract the second operand from the formula and store it in operand2
-
-    char *operand2 = strndup(formula + strlen(operand1) + 1, strlen(formula) - strlen(operand1) - 1);
-
-    //get the value of the first operand
-    //the operands only can be double type
-    //if the operand is not double type return -1 to indicate that the formula is not valid
-    //get the cell
-    cell *cell1 = matrix[atoi(operand1 + 1) ][char_to_enum(operand1[0])];
-
-    //check if the cell is double type
-    if (cell1->type != 0) {
+        printf("Invalid formula %s", formula);
         return -1;
     }
 
-    //get the value of the first operand
-    double value1 = cell1->d.i;
-
-    cell *cell2 = matrix[atoi(operand2 + 1)][char_to_enum(operand2[0])];
-    if (cell2->type != 0) {
+    char *token = strtok(NULL, " ");
+    char *op1 = token;
+    token = strtok(NULL, " ");
+    char *op = token;
+    token = strtok(NULL, " ");
+    char *op2 = token;
+    if (op == NULL || op1 == NULL || op2 == NULL) {
+        printf("Invalid formula %s", formula);
+        return -1;
+    }
+    if (op[0] != '+' && op[0] != '-' && op[0] != '*' && op[0] != '/') {
+        printf("Invalid formula %s", formula);
         return -1;
     }
 
-    double value2 = cell2->d.i;
+    double op1_d = 0;
+    double op2_d = 0;
 
-    //evaluate the formula and return the result
-    free(str1);
+    if (op1[0] >= 'A' && op1[0] <= 'J') {
+        if (matrix[row][char_to_enum(op1[0])]->type == 0) {
+            op1_d = matrix[row][char_to_enum(op1[0])]->d.i;
+        }
+        else {
+            printf("Invalid formula %s", formula);
+            return -1;
+        }
+    }
+    else {
+        op1_d = atof(op1);
+    }
+
+    if (op2[0] >= 'A' && op2[0] <= 'J') {
+        if (matrix[row][char_to_enum(op2[0])]->type == 0) {
+            op2_d = matrix[row][char_to_enum(op2[0])]->d.i;
+        }
+        else {
+            printf("Invalid formula %s", formula);
+            return -1;
+        }
+    }
+    else {
+        op2_d = atof(op2);
+    }
+
     double result = 0;
 
-    if (strcmp(operator, "+") == 0) {
-        result = value1 + value2;
-    } else if (strcmp(operator, "-") == 0) {
-        result = value1 - value2;
-    } else if (strcmp(operator, "*") == 0) {
-        result = value1 * value2;
-    } else if (strcmp(operator, "/") == 0) {
-        result = value1 / value2;
+    switch(op[0]) {
+        case '+':
+            result = op1_d + op2_d;
+            break;
+        case '-':
+            result = op1_d - op2_d;
+            break;
+        case '*':
+            result = op1_d * op2_d;
+            break;
+        case '/':
+            result = op1_d / op2_d;
+            break;
     }
 
-    //print value1, value2, operator, result
+    return result;
 
-    printf("value1: %lf\n", value1);
-    printf("value2: %lf\n", value2);
-    printf("operator: %s\n", operator);
-    printf("result: %lf\n", result);
-    return 0;
 }
 
-//Generate a function to save the grid to a file in the following format
-//The first column is the column name and the first row is the row number
-//Next to the column name and row number is the data type of the cell
-//The data type is represented by a number
-//0: double
-//1: string
-//2: formula
-void save_grid(char* file_name, cell*** matrix, int* size) {
-    FILE *fp = fopen(file_name, "w");
+void guardarPlanilla(char* file_name, cell*** matrix, int* size) {
+    //open the file for writing and read
+    FILE *fp = fopen(file_name, "w+");
     if (fp == NULL) {
         printf("Error: cannot open file for writing\n");
         return;
     }
-    for (int i = 0; i < size[0]; i++) {
-        for (int j = 0; j < size[1]; j++) {
+    int i,j;
+    for (i = 0; i < size[0]; i++) {
+        for (j = 0; j < size[1]; j++) {
             if (i == 0 && j == 0) {
-                fprintf(fp, "%c\t%d\t%d\t", enum_to_char(j), i, matrix[i][j]->type);
+                fprintf(fp, "%c%d %d %lf\n", enum_to_char(j), i, matrix[i][j]->type, matrix[i][j]->d.i);
             } else if (i == 0) {
-                fprintf(fp, "\n%c\t%d\t%d\t", enum_to_char(j), i, matrix[i][j]->type);
+                fprintf(fp, "%c%d %d %s\n", enum_to_char(j), i, matrix[i][j]->type, matrix[i][j]->d.s);
             } else if (j == 0) {
-                fprintf(fp, "%c\t%d\t%d\t", enum_to_char(j), i, matrix[i][j]->type);
+                fprintf(fp, "%c%d %d %lf\n", enum_to_char(j), i, matrix[i][j]->type, matrix[i][j]->d.i);
             } else {
-                fprintf(fp, "\t%c\t%d\t%d\t", enum_to_char(j), i, matrix[i][j]->type);
+                fprintf(fp, "%c%d %d %s\n", enum_to_char(j), i, matrix[i][j]->type, matrix[i][j]->d.s);
             }
-            if (matrix[i][j]->type == 0) {
-                fprintf(fp, "%lf", matrix[i][j]->d.i);
-            } else if (matrix[i][j]->type == 1) {
-                fprintf(fp, "%s", matrix[i][j]->d.s);
-            } else if (matrix[i][j]->type == 2) {
-                fprintf(fp, "%s", matrix[i][j]->d.s);
-            }
+            fprintf(fp, "\n");
         }
     }
+
+    //close the file and not forget to free the memory
+    if(fclose(fp) != 0) {
+        printf("Error: cannot close file\n");
+    }
+    printf("Grid saved successfully\n");
+
+    free_memory(matrix, size);
+
+}
+
+
+//Print the sheet from a file with the same format as the save_grid function
+void mostrarPlanilla(char* file_name) {
+    FILE *fp = fopen(file_name, "r");
+    if (fp == NULL) {
+        printf("Error: cannot open file for reading\n");
+        return;
+    }
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("%s", line);
+    }
     fclose(fp);
-    printf("Success: file saved\n");
+    if (line) {
+        free(line);
+    }
+}
+
+//menu function to allow the user to choose the operation
+//1: create a new sheet
+//2: add data to the sheet
+//3: get the value of a cell
+//4: evaluate the formula of a cell
+//5: save the sheet to a file
+//6: print the sheet from a file
+//7: exit the program
+void menu() {
+    int size[2];
+    cell*** matrix = NULL;
+    int choice = 0;
+    char file_name[100];
+    data d;
+
+    while (choice != 7) {
+        printf("1: create a new sheet\n2: add a data in a cell\n3: get the value of a cell\n4: evaluate a formula\n5: save the sheet to a file\n6: print the sheet\n7: exit\n");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter the number of rows and columns: ");
+                scanf("%d %d", &size[0], &size[1]);
+                if (size[0] > 10 || size[1] > 10) {
+                    printf("Error: the size of the sheet is too large\n");
+                    break;
+                }
+                matrix = crearPlanilla(size[0], size[1]);
+                break;
+            case 2:
+                if (matrix == NULL) {
+                    printf("Error: the sheet is not created\n");
+                    break;
+                }
+                char column;
+                int row;
+                int type;
+                data d;
+                printf("Enter the column, row, type, and data: ");
+                scanf(" %c %d %d", &column, &row, &type);
+                ingresarDato(char_to_enum(column), row, type, matrix);
+                break;
+            case 3:
+                if (matrix == NULL) {
+                    printf("Error: the sheet is not created\n");
+                    break;
+                }
+                printf("Enter the column and row: ");
+                scanf(" %c %d", &column, &row);
+
+                printf("The value of the cell is: %s\n", obtenerDato(char_to_enum(column), row, matrix));
+                break;
+            case 4:
+                if (matrix == NULL) {
+                    printf("Error: the sheet is not created\n");
+                    break;
+                }
+                char column1;
+                int row1;
+                printf("Enter the column and row: ");
+                scanf(" %c %d", &column1, &row1);
+                evaluarFormula(char_to_enum(column1), row1, matrix);
+                break;
+            case 5:
+                if (matrix == NULL) {
+                    printf("Error: the sheet is not created\n");
+                    break;
+                }
+                printf("Enter the file name: ");
+                scanf("%s", file_name);
+                guardarPlanilla(file_name, matrix, size);
+                break;
+            case 6:
+                printf("Enter the file name: ");
+                scanf("%s", file_name);
+                mostrarPlanilla(file_name);
+                break;
+            case 7:
+                printf("Goodbye!\n");
+                if (matrix != NULL) {
+                    free_memory(matrix, size[0]);
+                }
+                break;
+            default:
+                printf("Error: invalid choice\n");
+                break;
+        }
+    }
+}
+
+void print(cell*** matrix, int size[2]) {
+    int i, j;
+    for (i = 0; i < size[0]; i++) {
+        for (j = 0; j < size[1]; j++) {
+            if (matrix[i][j]->type == 0) {
+                printf("%lf ", matrix[i][j]->d.i);
+            }
+            else if (matrix[i][j]->type == 1) {
+                printf("%s ", matrix[i][j]->d.s);
+            }
+            else {
+                printf("%s ", matrix[i][j]->d.s);
+            }
+        }
+        printf("\n");
+    }
 }
 
 int main() {
-
-    int size[2] = {2, 2};
-    cell*** matrix = create_matrix(size[0], size[1]);
-    //create a random string for formula type
-    string s = (string)malloc(sizeof(char)*100);
-    strcpy(s, "=A0+B0");
-    matrix[1][0]->d.s = s;
-    matrix[1][0]->type = 2;
-    //Fill the rest of the grid with double type
-    for(int i=0; i<size[0]; i++) {
-        for (int j = 0; j < size[1]; j++) {
-            if (matrix[i][j]->type == 0) {
-                input_data(j, i, 0, matrix);
-            }
-        }
-    }
-
-    //print the grid
-    print_grid(matrix, size);
-
-    evaluate_formula('A', 1, matrix);
-
-    //save the grid to a file
-    save_grid("test.txt", matrix, size);
-
-    //free the memory
-    free_memory(matrix, size);
+    menu();
     return 0;
 }
